1defbad56f0124928207d9a07f00b304bfe0690d
Morgan Segura <morgansegura@gmail.com>
2016-12-06 16:20:15 -0800

    start working on js test

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,7 +11,37 @@
   'use strict';
 
   $(function () {
-    // FastShell
+    var quiz = [
+        {
+            'question': 'Explain "use strict";',
+            'answer': 'Wacka'
+        },
+        {
+            'question': 'What are the different types in in javascript?',
+            'answer': ''
+        },
+        {
+            'question': 'What does the function typeof() do?',
+            'answer': ''
+        },
+        {
+            'question': 'What does it mean that javascript is a dynamically typed language?',
+            'answer': ''
+        },
+        {
+            'question': 'What is the difference between "udnefined" and "null"?',
+            'answer': ''
+        }
+    ];
+
+    var question = document.getElementsByClassName('question-headline');
+    var answer = document.getElementsByClassName('answer-block');
+
+    for( var i = 1; i < quiz.length; i++) {
+        question[i].innerHTML = quiz[i].question;
+        answer.innerHTML = '<li>'+quiz[i].question+'</li>';
+    }
+
   });
 
 })(jQuery, window, document);
